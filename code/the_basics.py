5ed00ff2953c5706053c7ee5c0d2df32cecb1ecb
Roos van Velthoven <80839085+RoosvanVelthoven@users.noreply.github.com>
2021-10-05 09:56:16 +0100

    day one

---
+++ code/the_basics.py
@@ -0,0 +1,18 @@
+# Step one: install Python
+# https://www.python.org/
+
+# Note: Add Python to Path
+
+# Step two: install a code editor
+# Code editors are used to write code.
+
+# Note: My road to Python mastery will start with the code editor known as Pycharm.
+# https://www.jetbrains.com/pycharm/
+# Pycharm is considered an IDE (integrated development environment).
+# Pycharm has additional features which should make it easier to write code.
+
+# Step three: create a project and a new Python file.
+
+# Note: Python files by convention end in .py.
+
+
